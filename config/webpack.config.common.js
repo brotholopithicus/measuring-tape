26e77a605cfd1b5d3785d6ebd6af3e530a492cae
James Jarvis <jamespjarvis@gmail.com>
2017-08-28 15:12:44 -0400

    build

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, '../src'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, '../dist')
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'])
+  ]
 }
